49d0cd217183c956acd8a6e33afdeceb75dbe391
konrad <ktalaga90@gmail.com>
2021-04-27 01:13:15 +0100

    record updated with udpadeOne

--- mongodb_play.js
+++ mongodb_play.js
@@ -3,3 +3,8 @@ use zoo;
 
 const id = ObjectId('608747e651f029439899333b');
 db.animals.findOne({ _id: id });
+
+db.animals.updateOne( 
+    { _id: ObjectId('608747e651f029439899333b') },
+    { $set: { name: "Knurek" } }
+  );
